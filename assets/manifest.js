0908c69eecae47b0aef1f172a5afe667bc479b10
Julian Luzanilla <112124926+julianluzanilla@users.noreply.github.com>
2025-11-12 12:53:01 -0700

    Create manifest.js

---
+++ assets/manifest.js
@@ -0,0 +1,35 @@
+// /assets/manifest.js
+export const ASSETS = {
+  brand: {
+    logo:      'assets/brand/logo.svg',
+    logoini:   'assets/brand/logoini.svg',
+    logofin:   'assets/brand/logofin.svg',
+  },
+  avatars: {
+    male:      'assets/avatars/avatarh.svg',
+    female:    'assets/avatars/avatarm.svg',
+  },
+  icons: {
+    ui: {
+      menu:     'assets/icons/ui/menu.svg',
+      usuario:  'assets/icons/ui/usuario.svg',
+      password: 'assets/icons/ui/password.svg',
+      nuevo:    'assets/icons/ui/nuevo.svg',
+      editar:   'assets/icons/ui/editar.svg',
+      borrar:   'assets/icons/ui/borrar.svg',
+      filtro:   'assets/icons/ui/filtro.svg',
+      exportar: 'assets/icons/ui/exportar.svg',
+      respaldo: 'assets/icons/ui/respaldo.svg',
+      logout:   'assets/icons/ui/logout.svg',
+    },
+    types: {
+      bano:         'assets/icons/types/baño.svg',
+      dolor:        'assets/icons/types/dolor.svg',
+      lpm:          'assets/icons/types/lpm.svg',
+      mareo:        'assets/icons/types/mareo.svg',
+      medicamento:  'assets/icons/types/medicamento.svg',
+      sangre:       'assets/icons/types/sangre.svg',
+      peso:         'assets/icons/types/peso.svg',
+    }
+  }
+};
